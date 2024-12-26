420eb6149a3a876e3d2c10ac3241198b2675e747
Ognen <nikolovskiognen1@gmail.com>
2024-12-26 16:22:00 +0100

    First Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express, { json } from 'express';
+import bodyParser from 'body-parser';
+import {dirname} from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is on ${port}`);
+    });
