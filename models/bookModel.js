03834fafd1ad660344b083844e41cc6e0121fd8f
Ognen <nikolovskiognen1@gmail.com>
2025-03-24 23:05:26 +0100

    First Commit - just created empty migration and  model for book

---
+++ models/bookModel.js
@@ -0,0 +1,8 @@
+import { DataTypes } from 'sequelize'; // Imposting and using datatypes from sequelize
+import { sequelize } from '../database/dbConnect.js';
+
+
+
+const Book = new sequelize.define("Book", {
+    
+})
